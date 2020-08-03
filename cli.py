1d21c2e09ec086aba335e460ed3e516552637138
webhortiz <advancoplanet@gmail.com>
2020-08-03 15:51:16 +0100

    new commit

--- cli.py
+++ cli.py
@@ -6,13 +6,13 @@ from urllib.request import urlretrieve
 
 
 
-def search_string(query):
-    driver = webdriver.Chrome("c://chromedriver")
-    driver.get("https://www.google.co.in/search?q=" + query + "&source=lnms&tbm=isch")
+def search_string(query): #accept search string
+    driver = webdriver.Chrome("c://chromedriver") #init chrome driver
+    driver.get("https://www.google.co.in/search?q=" + query + "&source=lnms&tbm=isch") #get to  google
     page = driver.page_source
     soup = BeautifulSoup(page, 'html.parser')
-    mydivs = soup.findAll("img", {"class": "rg_i"})
-    return mydivs[0]["src"]
+    mydivs = soup.findAll("img", {"class": "rg_i"}) #find img classwith rg_i which is always first
+    return mydivs[0]["src"] #get image source base64
 
 
 def save_image(link):
@@ -20,7 +20,7 @@ def save_image(link):
     print('===Start downloading...===')
 
     try:
-        os.mkdir('downloads')
+        os.mkdir('downloads') #create folder downloads in current directory
     except FileExistsError:
         pass
         count = 0
@@ -28,15 +28,15 @@ def save_image(link):
     try:
         if src != None:
             src = str(src)
-            count = randint(1, 2000)
-            urlretrieve(src, os.path.join('downloads', 'image' + str(count) + '.jpg'))
+            count = randint(1, 2000) #generate random number
+            urlretrieve(src, os.path.join('downloads', 'image' + str(count) + '.jpg')) #specify image name
             print('==== Image Download and Stored in Downloads Folder =====')
         else:
             raise TypeError
     except TypeError:
         print('fail')
 
-@click.command()
+@click.command() #init click cli
 @click.option('--search', '-s', default="fish", help='Enter search value')
 def main(search):
 
